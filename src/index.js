0f0c30108b26431c6a11b9e3e8303c7ffa881da8
ihmccd <igbus@mail.ru>
2019-03-10 21:17:13 +0300

    fix: new solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-module.exports = function solveSudoku(matrix) {
+module.exports = function solveSudoku(arr) {
    
-  //converts 9 arrays in 1
-    
+  
   let strSud= [];
-  for (let i = 0; i< matrix.length; i++){
-    for (let j = 0; j< matrix[i].length; j++)
-      strSud.push(matrix[i][j]);
+  for (let i = 0; i< arr.length; i++){
+    for (let j = 0; j< arr[i].length; j++)
+      strSud.push(arr[i][j]);
   }
-
-  // positions of values with 0
+ //console.log(strSud);
   
   let zeroesArr =[];
   for (let i = 0; i< strSud.length; i++){
@@ -17,85 +15,87 @@ module.exports = function solveSudoku(matrix) {
    } 
   }
   
-  let failsArr = []; // detects ded end meanings
-
-  // builds row and column of 0 meaning
-
+  
+  let zerosValues = [];
+  zeroesArr.forEach(function(e){
+    zerosValues.push(getOptionValues(getRowsColumns(strSud,e)));
+  })
+  
+  
+  //console.log(zerosValues);
+  
   function getRowsColumns(arr,num){
     let arrSliceRow = arr.slice(Math.floor(num/9)*9,Math.floor(num/9)*9+9); 
+ 
     let arrSliceColumn = [];
-    let  colNumber = num%9;
+    let  rowNumber = num%9;
+    //console.log(rowNumber);
     
-    while (colNumber<arr.length){
-      if (arr[colNumber]!=0){
-        arrSliceColumn.push(arr[colNumber]);
+    while (rowNumber<arr.length){
+      if (arr[rowNumber]!=0){
+      arrSliceColumn.push(arr[rowNumber]);
       }
-      colNumber+=9;
+      rowNumber+=9;
     }
    
-   return [arrSliceRow, arrSliceColumn];
-    }
-  
-  
- // builds arr with possible replacement calues for 0 
 
+    //console.log([arrSliceRow, arrSliceColumn]);
+   return [arrSliceRow, arrSliceColumn];
+ }
+  
   function getOptionValues(arrRowColumn){
     let values = [1,2,3,4,5,6,7,8,9];
     let option = [];
+    //console.log(arrRowColumn[0]);
     values.forEach(function(e){
+      //console.log(e, arrRowColumn[0].indexOf(e), arrRowColumn[1].indexOf(e));
       if (arrRowColumn[0].indexOf(e)<0 && arrRowColumn[1].indexOf(e)<0){
         option.push(e);
       }
     })
+    //console.log(option);
     return option;
   }
   
-  // step back if dead end
   
-  function stepBack(arrAll, num){
-      failsArr[num]=[];
-      let numPrev  = zeroesArr[zeroesArr.indexOf(num)-1];
-      let exclude = arrAll[numPrev];
-    if (!failsArr[numPrev]){
-      failsArr[numPrev]=[];
-    }
-      failsArr[numPrev].push(exclude);
-      arrAll[numPrev] = 0;   
+
+  
+  
+  let solutionPath=[];
+  let deadEnds=[];
+  
+  
+  function stepBack(num){
+      let deadEnd=solutionPath.pop();
+      let prevNum = zeroesArr[zeroesArr.indexOf(num)-1];
+      strSud[prevNum]=0;
+      let prevNumOptions = getOptionValues(getRowsColumns(strSud,prevNum));
+      if(!prevNumOptions[prevNumOptions.indexOf(deadEnd)+1]){
+        stepBack(prevNum);
+      }
+      else{
+        strSud[prevNum]=prevNumOptions[prevNumOptions.indexOf(deadEnd)+1];
+        solutionPath.push(strSud[prevNum]);
+      }
+      
   }
   
   
-  // find meaning & try to replace; if dead end, step back to replace prev meaning
-  // recursion
-   
-  function step(arrAll){
-    let num = arrAll.indexOf(0);
-    
-    if (num<0){
-      return arrAll;
+  while(solutionPath.length<zeroesArr.length){
+    let num=strSud.indexOf(0);
+    //console.log(num);
+    let posValues = getOptionValues(getRowsColumns(strSud,num));
+    //console.log(posValues);
+    if(posValues.length===0){
+        stepBack(num);
+    }
+    else{
+     strSud[num]= posValues[0];
+     solutionPath.push(posValues[0]); 
+    }
     }
     
-    let optArr = getOptionValues(getRowsColumns(arrAll,num));
-    if (failsArr[num]){
-      failsArr[num].forEach(function(e){;
-        if(e){
-        optArr.splice(optArr.indexOf(e),1);
-    }
-      })
-    }
-    if (optArr.length==0){
-      stepBack(arrAll, num);
-      return step(arrAll);
-    }
-      arrAll[num]=optArr[0];
-      return step(arrAll);
-      
-
-}
-
-  let temp = step(strSud); // solution 1 long arr
-  
-  // edit long arr into 1 with 9 sub arrs like task description
-
+    
   function editedArr(arr){
     let edited = [];
     for (let i = 0; i< arr.length; i+=9){
@@ -104,7 +104,13 @@ module.exports = function solveSudoku(matrix) {
     }
     return edited;
   }
-
- return editedArr(temp);
-  //console.log(editedArr(tt));
+    
+    
+ // console.log(solutionPath, editedArr(strSud));
+  return editedArr(strSud);
 }
+
+
+
+  //console.log(editedArr(tt));
+
